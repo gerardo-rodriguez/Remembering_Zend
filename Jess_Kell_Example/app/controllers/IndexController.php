197e62ab25b8395927ee3489b663131853a61d67
Gerardo Rodriguez <grodriguez@cmdagency.com>
2011-02-10 17:12:48 -0800

    Adding the files.

---
+++ Jess_Kell_Example/app/controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+	class IndexController extends Zend_Controller_Action
+	{
+		public function init() {
+			//$this->session_alert = new Zend_Session_Namespace('');
+			//$this->Model = new Model();
+			
+			//Sets alternative layout
+			//$this->_helper->layout->setLayout('');
+			
+			//Access to helper
+			//$helper = $this->_helper->HelperName;
+		}
+		
+		public function indexAction()
+		{
+
+		}
+		
+	}
+	
+?>
---
+++ Jess_Kell_Example/app/models/Model.php
@@ -0,0 +1,7 @@
+<?php
+	Class Model extends Zend_Db_Table_Abstract
+	{
+		private $table = "";
+		protected $_name = "";
+	}
+?>
